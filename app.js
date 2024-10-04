755f199b1c134432dee7076ea2cd99f52a237bfb
Yehor Kurochkin <yehor.kurochkin@onicore.io>
2024-10-04 18:13:59 +0300

    test

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const responseBody = {
 }
 const responseHeaders = {}
 app.post('/debit/authorize', (req, res) => {
-    res.header("Access-Control-Allow-Headers", "x-signature, x-date");
     res.header("x-signature", "fe845227b2a12803948a2ea3da136a16b05e7fbefb429012c5483b5cf7ce5643ef1211287b977a29afbd6d7d67aaec6eb2ac1ddc348e5a5bf04832b548acfbcb");
     res.header('x-date', '1728038932096')
+    res.header('content-type', 'application/json')
     res.send(responseBody)
 })
 
