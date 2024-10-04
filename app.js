88a5cda5a0f5d495d5e00b0e5347382794646e4b
Yehor Kurochkin <yehor.kurochkin@onicore.io>
2024-10-04 17:15:59 +0300

    test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const responseBody = {
     "request_id": "ab3d89e4-3231-11ed-a261-0242ac120002"
 }
 const responseHeaders = {}
-app.post('/', (req, res) => {
+app.post('/debit/authorize', (req, res) => {
     res.set('Content-Type', 'application/json')
     res.set('x-signature', 'fe845227b2a12803948a2ea3da136a16b05e7fbefb429012c5483b5cf7ce5643ef1211287b977a29afbd6d7d67aaec6eb2ac1ddc348e5a5bf04832b548acfbcb')
     res.set('x-date', '1728038932096')
