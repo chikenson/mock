6f5851f7329fa2882feeaf54b232142d2ab6fde6
Yehor Kurochkin <yehor.kurochkin@onicore.io>
2024-10-04 18:19:33 +0300

    test

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express()
 const port = 4444
 
 const responseBody = {
-    "request_id": "ab3d89e4-3231-11ed-a261-0242ac120002"
+    request_id: "ab3d89e4-3231-11ed-a261-0242ac120002"
 }
 const responseHeaders = {}
 app.post('/debit/authorize', (req, res) => {
     res.header("x-signature", "fe845227b2a12803948a2ea3da136a16b05e7fbefb429012c5483b5cf7ce5643ef1211287b977a29afbd6d7d67aaec6eb2ac1ddc348e5a5bf04832b548acfbcb");
     res.header('x-date', '1728038932096')
     res.header('content-type', 'application/json')
-    res.send(responseBody)
+    res.json(responseBody)
 })
 
 app.listen(port, () => {
